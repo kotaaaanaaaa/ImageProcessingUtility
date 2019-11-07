bbbd6c88d87dc306bf3239e8965b96d61a2dfd03
kota_na_ <ad.maiora.1@gmail.com>
2019-11-08 01:43:17 +0900

    feat: ヒストグラム表示

--- ImageProcessingUtility/ImageProcessModel.cs
+++ ImageProcessingUtility/ImageProcessModel.cs
@@ -1,13 +1,15 @@
 ﻿using ImageProcessingUtility.Processor;
+using LiveCharts;
 using OpenCvSharp;
 using System.Collections.ObjectModel;
-using System.Collections.Specialized;
 
 namespace ImageProcessingUtility
 {
     public class ImageProcessModel
     {
-        public Mat Source;//TODO Notify
+        public Mat Source { get; set; }
+
+        public Mat Result { get; set; }
 
         public ObservableCollection<IProcess> Processes { get; } = new ObservableCollection<IProcess>();
 
@@ -21,14 +23,52 @@ namespace ImageProcessingUtility
             Processes.Add(process);
         }
 
-        public Mat Process()
+        public static ChartValues<double> CalcHistogram(Mat img)
+        {
+            if (img.Channels() != 1)
+            {
+                return new ChartValues<double>();
+            }
+            return CalcHistogramInternal(img, 0);
+        }
+
+        public static ChartValues<double> CalcHistogram(Mat img, int channel)
+        {
+            if (img.Channels() == 1)
+            {
+                return new ChartValues<double>();
+            }
+            return CalcHistogramInternal(img, channel);
+        }
+
+        private static ChartValues<double> CalcHistogramInternal(Mat img, int channel)
+        {
+            var result = new ChartValues<double>();
+
+            if (channel >= img.Channels())
+            {
+                return result;
+            }
+
+            var histogram = new Mat();
+            Cv2.CalcHist(new[] { img }, new[] { channel }, new Mat(), histogram, 1, new[] { 256 }, new[] { new float[] { 0, 256 } });
+
+
+            for (var i = 0; i < histogram.Rows; i++)
+            {
+                result.Add(histogram.GetArray(i, 0)[0]);
+            }
+            return result;
+        }
+
+        public void Process()
         {
             var image = Source;
             foreach (var ip in Processes)
             {
                 image = ip.Process(image);
             }
-            return image;
+            Result = image;
         }
     }
 }
--- ImageProcessingUtility/MainWindowViewModel.cs
+++ ImageProcessingUtility/MainWindowViewModel.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.ComponentModel;
-using System.Timers;
-using ImageProcessingUtility.Processor;
+﻿using ImageProcessingUtility.Processor;
+using LiveCharts;
 using OpenCvSharp.Extensions;
+using System.ComponentModel;
 using System.Windows.Media.Imaging;
 
 namespace ImageProcessingUtility
@@ -11,15 +10,50 @@ namespace ImageProcessingUtility
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public ImageProcessModel IPModel { get; set; }
-
         public BitmapSource SourceImage { get => IPModel.Source.ToBitmap().ToSource(); }
 
-        public BitmapSource ResultImage { get => IPModel.Process().ToBitmap().ToSource(); }
+        public BitmapSource ResultImage
+        {
+            get
+            {
+                IPModel.Process();
+                Analyze();
+                return IPModel.Result.ToBitmap().ToSource();
+            }
+        }
+
+        public ImageProcessModel IPModel { get; set; }
+
+        public ChartValues<double> SourceBlueHistogram { get => ImageProcessModel.CalcHistogram(IPModel.Source, 0); }
+
+        public ChartValues<double> SourceGreenHistogram { get => ImageProcessModel.CalcHistogram(IPModel.Source, 1); }
+
+        public ChartValues<double> SourceRedHistogram { get => ImageProcessModel.CalcHistogram(IPModel.Source, 2); }
+
+        public ChartValues<double> SourceHistogram { get => ImageProcessModel.CalcHistogram(IPModel.Source); }
+
+        public ChartValues<double> ResultBlueHistogram { get => ImageProcessModel.CalcHistogram(IPModel.Result, 0); }
+
+        public ChartValues<double> ResultGreenHistogram { get => ImageProcessModel.CalcHistogram(IPModel.Result, 1); }
+
+        public ChartValues<double> ResultRedHistogram { get => ImageProcessModel.CalcHistogram(IPModel.Result, 2); }
+
+        public ChartValues<double> ResultHistogram { get => ImageProcessModel.CalcHistogram(IPModel.Result); }
 
         public void Refresh()
         {
             PropertyChanged.Raise(() => ResultImage);
+            Analyze();
+        }
+
+        public void Analyze()
+        {
+            PropertyChanged.Raise(() => SourceBlueHistogram);
+            PropertyChanged.Raise(() => SourceGreenHistogram);
+            PropertyChanged.Raise(() => SourceRedHistogram);
+            PropertyChanged.Raise(() => ResultBlueHistogram);
+            PropertyChanged.Raise(() => ResultGreenHistogram);
+            PropertyChanged.Raise(() => ResultRedHistogram);
         }
     }
 
